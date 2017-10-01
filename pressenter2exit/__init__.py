276ddf6fb29380c250caf5d865d7c9f7836a19a9
james abel <j@abel.co>
2017-09-30 21:22:17 -0700

    expand time methods

--- pressenter2exit/__init__.py
+++ pressenter2exit/__init__.py
@@ -2,25 +2,39 @@
 import time
 import threading
 
-__version__ = '0.0.7'
+__version__ = '0.0.8'
 
 
 class PressEnter2Exit(threading.Thread):
     def __init__(self):
         super().__init__(daemon=True)
         self.start_time = time.time()
-        self.end_time = None
+        self.exit_time = None
         self.start()
 
     def run(self):
         input('Press enter to exit:')
-        self.end_time = time.time()
+        self.exit_time = time.time()
 
-    def get_runtime(self):
+    def get_start_time(self):
         """
-        returns run time in seconds
-        :return: run time in seconds
+        get time of instantiation (in seconds since epoch)
+        :return: time of instantiation (in seconds since epoch)
         """
-        if self.end_time is None:
-            self.end_time = time.time()
-        return self.end_time - self.start_time
+        return self.start_time
+
+    def get_exit_duration(self):
+        """
+        time from instantiation to when enter was pressed
+        :return: time in seconds from instantiation to when enter was pressed or None if enter not yet pressed
+        """
+        if self.exit_time is None:
+            return None
+        return self.exit_time - self.start_time
+
+    def get_duration(self):
+        """
+        time since instantiation
+        :return: time in seconds since instantiation
+        """
+        return time.time() - self.start_time
