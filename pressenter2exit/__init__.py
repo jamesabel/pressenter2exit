2c5bfcb3e6e23e6a43e927d99e67561f72b6d0de
james abel <j@abel.co>
2017-09-30 19:45:11 -0700

    add run time

--- pressenter2exit/__init__.py
+++ pressenter2exit/__init__.py
@@ -1,13 +1,28 @@
 
+import time
 import threading
 
-__version__ = '0.0.5'
+__version__ = '0.0.6'
 
 
 class PressEnter2Exit(threading.Thread):
     def __init__(self):
         super().__init__(daemon=True)
+        self.start_time = time.time()
+        self.end_time = None
         self.start()
 
     def run(self):
         input('Press enter to exit:')
+        self.end_time = time.time()
+
+    def get_runtime(self):
+        """
+        returns run time in seconds
+        :return: run time in seconds
+        """
+        if self.end_time is None:
+            # not done yet
+            return None
+        return self.end_time - self.start_time
+
--- test/example_runtime.py
+++ test/example_runtime.py
@@ -1,11 +1,14 @@
 
-# a very simple example of using pressenter2exit
-
 from time import time, sleep
 
 from pressenter2exit import PressEnter2Exit
 
 exit_control = PressEnter2Exit()
+
+# optional, but makes sure things are on separate lines
+sleep(0.1)
+print()
+sleep(0.1)
 
 start_time = time()
 
@@ -15,3 +18,4 @@ while exit_control.is_alive() and time() - start_time < 20.0:
     exit_control.join(4.0)  # use join() instead of time.sleep() to ensure an immediate exit
 
 print('Done! Exiting after %f seconds.' % (time() - start_time))
+print('We ran for %f seconds' % exit_control.get_runtime())
