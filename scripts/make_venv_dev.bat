0f7f64117e0ee1346f88ae93cda5cb73f8edab4c
JamesAbel <j@abel.co>
2024-03-03 14:43:54 -0800

    add script to make the venv

---
+++ scripts/make_venv_dev.bat
@@ -0,0 +1,8 @@
+pushd .
+cd ..
+rmdir /S /Q venv
+C:"\Program Files\Python312\python.exe" -m venv --clear venv
+venv\Scripts\python.exe -m pip install --no-deps --upgrade pip
+venv\Scripts\pip3 install -U setuptools
+venv\Scripts\pip3 install -U -r requirements-dev.txt
+popd
